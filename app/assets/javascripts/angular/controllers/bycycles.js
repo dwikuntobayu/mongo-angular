4fab1ef2f95b9d3a4fd48fb3deec5bacfc4c50e5
mohamad_dwikuntobayu <dwikunto.bayu@kiranatama.com>
2014-10-30 16:25:10 +0700

    Add new directive For insert lement to API

--- app/assets/javascripts/angular/controllers/bycycles.js
+++ app/assets/javascripts/angular/controllers/bycycles.js
@@ -5,6 +5,12 @@ bycycle.controller('BycyclesController', ['$scope', 'ProcessApi', 'ProcessUpdate
     $scope.expression_me = "Bycycle controller Loaded";
     $scope.bind_me = "Force beyond";
     $scope.list_bycycle = ProcessApi.query();
+    $scope.bycycleData = {};
+    $scope.bycycleData.hero = 'Hero Zero';
+
+    $scope.see_content = function(id_params, element_obj){
+      window.alert(id_params + " -> " + element_obj);
+    }
 
     $scope.promo = function(id) {
       $scope.bind_me = id;
@@ -13,9 +19,8 @@ bycycle.controller('BycyclesController', ['$scope', 'ProcessApi', 'ProcessUpdate
 
     $scope.process_update = function(id, brand_params) {
       $scope.bycycle_scope = ProcessUpdateApi.get({bycycleId: id});
-      var bycycle = ProcessUpdateApi.get({bycycleId: id});
-      bycycle.brand = brand_params;
-
+      // var bycycle = ProcessUpdateApi.get({bycycleId: id});
+      // bycycle.brand = brand_params;
       //bycycle.$update({id: id}, bycycle);
 
       ProcessUpdateApi.update({id: id, brand: brand_params});
@@ -26,6 +31,22 @@ bycycle.controller('BycyclesController', ['$scope', 'ProcessApi', 'ProcessUpdate
 
   }
 ]);
+
+bycycle.directive('getValueElement', function(){
+  return function(scope, element, attributes) {
+    element.bind("click", function(){
+      scope.see_content($(this).attr('id'), $(this).text());
+    });
+  }
+});
+
+bycycle.directive('processValueUpdate', function(){
+  return function(scope, element, attributes) {
+    element.bind("blur", function(){
+      scope.process_update($(this).attr('id'), $(this).text());
+    });
+  }
+});
 
 bycycle.directive('cstEnter', function() {
         return function(scope, element, attrs) {
